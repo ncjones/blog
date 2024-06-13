28e6ef9054250b3225619795ab8df60a5b7b67b2
Nathan Jones <nathan@ncjones.com>
2024-06-14 09:03:57 +1200

    Render draft posts

--- src/data/post.ts
+++ src/data/post.ts
@@ -3,48 +3,51 @@ type MarkdownInstance = import('astro').MarkdownInstance<any>;
 const { MODE } = import.meta.env;
 
 export type Post = {
-	title: string,
-	slug: string,
-	desc: string,
-	author: string,
-	timestamp: number,
-	draft: boolean,
-	date: string,
-	file: URL,
-	img: URL,
+  title: string,
+  slug: string,
+  desc: string,
+  author: string,
+  timestamp: number,
+  draft: boolean,
+  date: string,
+  file: URL,
+  img: URL,
 }
 
-export function single(post: MarkdownInstance): Post {
-	const slug = post.file.split('/').reverse()[0].replace('.md', '');
-	return {
-		...post.frontmatter,
-		Content: post.Content,
-		slug: slug,
-		draft: post.file.split('/').reverse()[1] === 'drafts',
-		timestamp: (new Date(post.frontmatter.date)).valueOf()
-	}
+function createPost(post: MarkdownInstance): Post {
+  const slug = post.file.split('/').reverse()[0].replace('.md', '');
+  return {
+    ...post.frontmatter,
+    Content: post.Content,
+    slug: slug,
+    draft: post.file.split('/').reverse()[1] === 'drafts',
+    timestamp: (new Date(post.frontmatter.date)).valueOf()
+  }
 }
 
-export function published(posts: MarkdownInstance[]): Post[] {
-	return posts
-		.filter(post => post.frontmatter.title)
-		.map(post => single(post))
-		.filter(post => MODE === 'development' || !post.draft)
-		.sort((a, b) => b.timestamp - a.timestamp)
+export function allPosts(posts: MarkdownInstance[]): Post[] {
+  return posts
+    .filter(post => post.frontmatter.title)
+    .map(createPost)
+    .sort((a, b) => b.timestamp - a.timestamp);
+}
+
+export function publishedPosts(posts: MarkdownInstance[]): Post[] {
+  return allPosts(posts).filter(post => !post.draft);
 }
 
 export function getRSS(posts: MarkdownInstance[]) {
-	return {
-		title: 'Astro Blog',
-		description: 'Astro Blog Feed',
-		stylesheet: true,
-		customData: `<language>en-us</language>`,
-		items: published(posts).map((post: Post) => ({
-			title: post.title,
-			description: post.desc,
-			link: post.slug,
-			pubDate: post.date,
-		})),
-	}
+  return {
+    title: 'Astro Blog',
+    description: 'Astro Blog Feed',
+    stylesheet: true,
+    customData: `<language>en-us</language>`,
+    items: publishedPosts(posts).map((post: Post) => ({
+      title: post.title,
+      description: post.desc,
+      link: post.slug,
+      pubDate: post.date,
+    })),
+  }
 }
 
